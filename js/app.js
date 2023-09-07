b606474584d4b4c29590db0b2551991cd02ee2c1
LucasCorbalan <lucas.corbalan.23@gmail.com>
2023-09-07 16:13:28 -0300

    uploading commit to the cloud

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
 const ComenzarJuego = () => {
   let ContenedorPadre = document.querySelector("#ContenedorPadre");
-  if (ContenedorPadre.innerHTML === "COMENZAR EL JUEGO") {
-    return (ContenedorPadre.innerHTML += `
+  return (ContenedorPadre.innerHTML += `
     <input id="input" class="container my-2" type="number" placeholder="ADIVINA CUAL ES EL NUMERO MAGICO ENTRE EL 1 Y EL 100" maxlength="2" required>
 </div><br><button class="btn btnEnviar">ENVIAR RESPUESTA</button>`);
-  }
 };
 
-const numerorandom = () =>{
-    let input = document.querySelector('#input');
-    if (numerorandom > 50){
-            
-    } else {
-        
-    }
-}
+const numerorandom = () => {
+  let input = document.querySelector("#input");
+  if (numerorandom > 50) {
+  } else {
+  }
+};
