350bf07336c923d956afde9bcd5d27ac2090f5e0
LucasCorbalan <lucas.corbalan.23@gmail.com>
2023-09-11 15:41:30 -0300

    random number command added

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 const ComenzarJuego = () => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  console.log(randomNumber);
   let ContenedorPadre = document.querySelector("#ContenedorPadre");
   return (ContenedorPadre.innerHTML += `
     <input id="input" class="container my-2" type="number" placeholder="ADIVINA CUAL ES EL NUMERO MAGICO ENTRE EL 1 Y EL 100" maxlength="2" required>
-</div><br><button class="btn btnEnviar">ENVIAR RESPUESTA</button>`);
+</div><br><button class="btn btnEnviar" type="button" onclick='EnviarRespuesta()' id="BtnEnviar">ENVIAR RESPUESTA</button>`);
 };
 
-const numerorandom = () => {
-  let input = document.querySelector("#input");
-  if (numerorandom > 50) {
-  } else {
-  }
+const EnviarRespuesta = () => {
+  let BtnEnviar = document.querySelector("#BtnEnviar");
 };
